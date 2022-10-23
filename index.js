a3cc92b6760dc11cc4d68e20b752b4c98702e683
Mahmoud Galal <ssss.aaa2002@gmail.com>
2022-10-23 22:19:12 +0200

    support date on each data

--- index.js
+++ index.js
@@ -10,20 +10,32 @@ fetch('https://gdaask.azurewebsites.net/api/getAll')
     });
 })
 
+function dateFormat(date){
+    const d=new Date(date);
+    let h=d.getHours();
+
+    if(h>12){
+        h-=12
+    }
+    const m=d.getMinutes();
+    return `${h}:${m}`
+}
 
 function createData(element){
         const box=document.createElement('div')
         const btn=document.createElement('button')
         btn.textContent='Present'
-        
+        const date=document.createElement('p')
         const question=document.createElement('h2')
         const name=document.createElement('p')
         question.textContent=element.question;
         name.textContent=element.name;
+        date.textContent=dateFormat(element.date);
         btn.setAttribute('onclick',`saveToLocal('${element.name}','${element.question}')`)
         box.appendChild(question)
         box.appendChild(name)
         box.appendChild(btn)
+        box.appendChild(date)
         content.append(box)
 }
 
